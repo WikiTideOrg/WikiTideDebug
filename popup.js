8facce7c6db3c9dbca6bb17421b4167b58d5e363
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-04 21:54:49 -0700

    Cleanup code
    Signed-off-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- popup.js
+++ popup.js
@@ -1,51 +1,49 @@
-document.addEventListener( 'DOMContentLoaded', async function () {
-  'use strict';
+document.addEventListener('DOMContentLoaded', async () => {
+	'use strict';
 
-  var $options = [].slice.call( document.querySelectorAll( '.option' ) );
+	const $options = Array.from(document.querySelectorAll('.option'));
 
-  function onUpdate() {
-      let state = { action: 'set' };
+	function onUpdate() {
+		const state = { action: 'set' };
 
-      $options.forEach( function ( $el ) {
-          state[ $el.id ] = $el.checked !== undefined
-            ? $el.checked
-            : $el.value;
-      } );
+		$options.forEach(($el) => {
+			state[$el.id] = $el.checked !== undefined ? $el.checked : $el.value;
+		});
 
-      chrome.runtime.sendMessage( state );
-  }
+		chrome.runtime.sendMessage(state);
+	}
 
-  async function getAccessKey() {
-      const result = await chrome.storage.local.get( [ 'accessKey' ] );
-      return result.accessKey || '';
-  }
+	async function getAccessKey() {
+		const { accessKey } = await chrome.storage.local.get(['accessKey']);
+		return accessKey || '';
+	}
 
-  // Handling for access key input
-  var accessKeyInput = document.getElementById( 'accessKey' );
-  accessKeyInput.addEventListener( 'input', function () {
-      chrome.storage.local.set( { accessKey: accessKeyInput.value } );
-      onUpdate(); // Trigger the update function when access key changes
-  } );
+	// Handling for access key input
+	const accessKeyInput = document.getElementById('accessKey');
+	accessKeyInput.addEventListener('input', () => {
+		chrome.storage.local.set({ accessKey: accessKeyInput.value });
+		onUpdate(); // Trigger the update function when the access key changes
+	});
 
-  accessKeyInput.value = await getAccessKey();
-  
-  chrome.runtime.sendMessage( { action: 'get' }, function ( response ) {
-      $options.forEach( async function ( $el ) {
-          let value = response[ $el.id ];
+	accessKeyInput.value = await getAccessKey();
 
-          if ( typeof value === 'boolean' ) {
-              $el.checked = value;
-          } else {
-              if ( $el.id === 'accessKey' ) {
-                  value = await getAccessKey();
-              }
+	chrome.runtime.sendMessage({ action: 'get' }, (response) => {
+		$options.forEach(async ($el) => {
+			let value = response[$el.id];
 
-              $el.value = value;
-          }
+			if (typeof value === 'boolean') {
+				$el.checked = value;
+			} else {
+				if ($el.id === 'accessKey') {
+					value = await getAccessKey();
+				}
 
-          $el.addEventListener( 'change', onUpdate, false );
-      } );
+				$el.value = value;
+			}
 
-      document.body.className = '';
-  } );
-} );
+			$el.addEventListener('change', onUpdate, false);
+		});
+
+		document.body.className = '';
+	});
+});
