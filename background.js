978693c550444c3aa361afc866dd25590aaf5f6c
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-02 19:32:39 -0700

    Fix

--- background.js
+++ background.js
@@ -21,7 +21,7 @@ const debug = {
 	enabled: false,
 
 	// To which backend shall the request go to?
-	backend: 'test21.wikitide.net',
+	backend: 'test1.wikitide.net',
 
 	// Toggle state.
 	toggle: (state) => {
